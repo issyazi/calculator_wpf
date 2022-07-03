d373eb1708f9522a1b385f281847d13ecc3b33b0
issyazi <91420149+issyazi@users.noreply.github.com>
2022-07-04 06:20:55 +1000

    first commit

---
+++ calculator_wpf/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_wpf
+{
+    public class Calculator
+    {
+        public double Accum { get; private set;  }
+        public double Operand { get; private set; }
+        public string Value { get; private set; }
+        public enum State { FirstOp, SecondOp, Procent, Reciproc, Sqrt, SignChng }
+        public State state { get; private set; } = State.FirstOp;
+        public void Plus()
+        {
+            if (state == State.FirstOp) 
+            { 
+                Accum = double.Parse(Value);
+                Value = "";
+                state = State.SecondOp;
+             }
+            else
+            {
+                Operand = double.Parse(Value);
+                Accum += Operand;
+                Value = Accum.ToString();
+            }
+        }
+        public void Digit(string digit) //добавление чисел
+        {
+            if (state == State.SecondOp) { Value = ""; }
+            Value += digit;
+        }
+        public void Equal() //равно
+        {
+            Value = Convert.ToString(Accum);
+            state = State.FirstOp;
+        }
+        public void Erase()
+        {
+            Value = Value.Remove(Value.Length - 1);
+        }
+    }
+}
---
+++ calculator_wpf/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculator_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Calculator calc = new Calculator();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickDigit(object sender, RoutedEventArgs e)
+        {
+            calc.Digit((sender as Button).Content.ToString());
+            TextInput.Text = calc.Value;
+        }
+        private void ClickPlus(object sender, RoutedEventArgs e)
+        {
+            TextOutput.Text += calc.Value + " + ";
+            calc.Plus();
+            TextInput.Text = calc.Value;
+        }
+        private void ClickEqual(object sender, RoutedEventArgs e)
+        {
+            calc.Equal();
+            TextOutput.Text = "";
+            TextInput.Text = calc.Value;
+        }
+        private void ClickErase(object sender, RoutedEventArgs e)
+        {
+            calc.Erase();
+            TextInput.Text = calc.Value;
+        }
+        private void MemoryAdd(object sender, RoutedEventArgs e)
+        {
+            var m = new Label();
+            /*m.Width = 30;
+            m.Height = 30;
+            m.Content = "M";
+            MainGrid.Children.Add(m);*/
+        }
+        
+    }
+}
